22fdf05e6bb0e7d086a535336976c3c84f902340
cororing <corock.dev@gmail.com>
2021-02-02 13:26:08 +0900

    sprintf 활용 예제 - 정수형을 문자열로 사용하기

---
+++ library-functions/sprintf.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef struct _ {
+    char name[20];
+    int player_number;
+} SpursPlayer;
+
+int main()
+{
+    SpursPlayer sp1 = { "sonheongmin", 7 };
+    char temp[10];
+
+    printf("%s, %d\n", sp1.name, sp1.player_number);
+
+    // 런타임 에러(문자열 출력 함수의 인자 값으로 정수형이 들어감)
+    // puts(sp1.player_number);
+
+    /**
+     * sprintf: 정수형을 string 포맷으로 출력해주는 함수
+     *  e.g. 7 -> "7"
+     */
+    sprintf(temp, "%d", sp1.player_number);
+    printf("player_number(not int but char []): %s\n", temp);
+
+    return 0;
+}
